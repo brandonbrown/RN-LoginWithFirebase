27641cfbe2efb62a6c34371acbad447ad9742abb
Brandon Brown <temp@B-Wing.local>
2016-04-21 09:33:58 -0400

    update android

--- index.android.js
+++ index.android.js
@@ -1,51 +1,82 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-
+'use strict';
 import React, {
   AppRegistry,
   Component,
-  StyleSheet,
   Text,
-  View
+  View,
+  Navigator,
+  AsyncStorage
 } from 'react-native';
 
-class login extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+import Signup from './app/SignUp';
+import Account from './app/Account';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+import Header from './app/components/Header';
+
+import Firebase from 'firebase';
+
+let app = new Firebase("https://rn-login.firebaseio.com/");
+
+import styles from './app/util/styles.js';
+
+class login extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      component: null,
+      loaded: false
+    };
+  }
+
+  componentWillMount(){
+
+
+    AsyncStorage.getItem('user_data').then((user_data_json) => {
+      let user_data = JSON.parse(user_data_json);
+      let component = {component: Signup};
+      if(user_data != null){
+        app.authWithCustomToken(user_data.token, (error, authData) => {
+          if(error){
+            this.setState(component);
+          }else{
+            this.setState({component: Account});
+          }
+        });
+      }else{
+        this.setState(component);
+      }
+    });
+
+  }
+
+  render(){
+
+    if(this.state.component){
+      return (
+        <Navigator
+          initialRoute={{component: this.state.component}}
+          configureScene={() => {
+            return Navigator.SceneConfigs.FloatFromRight;
+          }}
+          renderScene={(route, navigator) => {
+            if(route.component){
+              return React.createElement(route.component, { navigator });
+            }
+          }}
+        />
+      );
+    }else{
+      return (
+        <View style={styles.container}>
+          <Header text="React Native Firebase Auth" loaded={this.state.loaded} />
+          <View style={styles.body}></View>
+        </View>
+      );
+    }
+
+  }
+
+}
 
 AppRegistry.registerComponent('login', () => login);
